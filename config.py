d7a583c848d052cb6f0809128fa89e7dc0f26e98
Muhammedshifin <muhammedshifink4@gmail.com>
2025-01-20 19:07:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,12 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5835387353:AAGvl4hBUgWcXoqe9bWjlDIvalKCEBcyRDk")
 API_ID = int(os.environ.get("API_ID", "18799477"))
 API_HASH = os.environ.get("API_HASH", "305cfa73e8b06daa97573809532034e5")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "5781922761,1957296068"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "578192276"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://jloverpmc:OpyVDcmfekfH5oCm@cluster0.p7col.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "filesharingbotnew")
 JOIN_REQ_DB = os.environ.get("JOIN_REQ_DB", DB_URL)
@@ -30,8 +28,8 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
 try:
-    ADMINS=[5781922761,1957296068]
-    for x in (os.environ.get("ADMINS", "5781922761,1957296068").split()):
+    ADMINS = [5781922761, 1957296068]
+    for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")

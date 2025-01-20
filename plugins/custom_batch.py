e00421096b505445f8cd1e4a2e7eb52381156936
Muhammedshifin <muhammedshifink4@gmail.com>
2025-01-20 20:21:56 +0530

    Update custom_batch.py

--- plugins/custom_batch.py
+++ plugins/custom_batch.py
@@ -1,6 +1,10 @@
-from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import base64
+from pyrogram import Client, filters
+from bot import Bot
+from config import ADMINS
+from helper_func import encode, get_message_id
+
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('custom_batch'))
 async def custom_batch(client: Client, message: Message):
